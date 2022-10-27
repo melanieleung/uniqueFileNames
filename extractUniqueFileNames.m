e1d55ada1cb3eec6f0bbbfedd7f2e310166fa681
Melanie Leung <49533829+melanieleung@users.noreply.github.com>
2022-10-27 13:11:49 -0700

    Update extractUniqueFileNames.m
    This updated script was modified in order to loop through the neural network training data subfolders (each subfolder representing a single site), and extract unique file names from the "finalOutputs" subfolder within each site.

--- extractUniqueFileNames.m
+++ extractUniqueFileNames.m
@@ -1,12 +1,13 @@
-mainFolder = "Y:\Planet_kelp\california\miscellaneous\CNN\trainingData_final\baja\islaNavidad\"; % main folder path
+mainFolder = "C:\Users\mleung\Desktop\trainingDataFinal\"; % main folder path
 mainFolderContents = dir(fullfile(mainFolder,'*')); % lists everything inside the main folder
 subfolders = setdiff({mainFolderContents([mainFolderContents.isdir]).name},{'.','..'}); % list of subfolders of mainFolder
 
-% loop through all the regional subfolders in the "regions" folder and
-% locates its respective "surfaceReflectance" subfolder
+% loops through all the subfolders in the main folder,
+% locates a specific subfolder "final outputs" within the different site
+% subfolders
 array = [];
 for i = 1:numel(subfolders)
-    cd(strcat(mainFolder, subfolders{i}, 'finalOutputs\')) % subfolder name of interest
+    cd(strcat(mainFolder, subfolders{i}, '\finalOutputs\')) % subfolder name of interest
     files= dir('*.tif'); % specifies unique pattern string that you're interested in extracting out of the folders
     fileNames = {files.name};
     fileNameInfo = ~startsWith(fileNames,'.');
